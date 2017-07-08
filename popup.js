defce98ed04536cfe7a7c9bb5b85dfc75cc94eeb
Grayson Wright <wright.grayson@gmail.com>
2017-07-08 01:57:00 -0400

    Generate & store a UUID to identify the extension

--- popup.js
+++ popup.js
@@ -2,24 +2,41 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
   if (request.action == "source") {
     message.innerText = "Sending to Assemble..."
 
-    var data = {
-      "query": "mutation ($data: ArbitraryObject!, $extension_uuid: String!){\n  create_chrome_extension_event(\n    extension_uuid: $extension_uuid,\n    data: $data\n    ) {\n    data\n  }\n}",
-      "variables":{
-        "data":{
-          "html": request.source
-        },
-        extension_uuid: "aaaa-bbbb-cccc-dddd-eeee",
+    chrome.storage.local.get("key", function(storage) {
+      var data = {
+        "query": "mutation ($data: ArbitraryObject!, $extension_uuid: String!){\n  create_chrome_extension_event(\n    extension_uuid: $extension_uuid,\n    data: $data\n    ) {\n    data\n  }\n}",
+        "variables":{
+          "data":{
+            "html": request.source
+          },
+          extension_uuid: storage.key,
+        }
       }
-    }
 
-    $.post("https://assembleapp.co/api", data, function() {
-      message.innerText = "Page HTML sent to Assemble"
+      console.log("Sending HTML to Assemble")
+      console.log(data)
+
+      $.post("https://assembleapp.co/api", data, function() {
+        message.innerText = "Page HTML sent to Assemble"
+      })
     })
   }
 });
 
 function onWindowLoad() {
   var message = document.querySelector('#message');
+  var uuid = document.querySelector("#uuid");
+
+  chrome.storage.local.get("key", function(data) {
+    var key = data.key
+
+    if(key == null) {
+      key = uuidv4()
+      chrome.storage.local.set({key: key});
+    }
+
+    uuid.innerText = key
+  });
 
   chrome.tabs.executeScript(null, {
     file: "getPageSource.js"
@@ -33,3 +50,11 @@ function onWindowLoad() {
 }
 
 window.onload = onWindowLoad;
+
+
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
